55fec7d6169ec9b06341eec0d3fb65bf545860c0
JarvisSan22 <jarvissan21@gmail.com>
2021-02-20 08:54:38 +0900

    First Html temp

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 08:14:44 2021
+
+@author: dev
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
